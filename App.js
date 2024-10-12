cfc0e22ce02283024006f75d33796c38d300ffad
Lychelle <lychelle.pham@ucalgary.ca>
2024-10-12 15:12:47 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, {useLayoutEffect, useState} from "react";
+
+const App = () => {
+    return <canvas id="canvas" style={{backgroundColor: blue}} 
+    width={window.innerWidth}
+    height={window.innerHeight}>Canvas</canvas>
+};
+
+export default App;
+//console.log("Hello World!");
