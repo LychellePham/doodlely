6178ed56364966d0ee1db020307f3cfd01d2cf84
Lychelle <lychellepham731@gmail.com>
2024-11-30 00:13:34 -0700

    update rectangle tool functionality

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
-const canvas = document.querySelector("canvas"),
-ctx = canvas.getContext("2d");
+const canvas = document.querySelector("canvas");
+const toolButtons = document.querySelectorAll(".tool");
+const ctx = canvas.getContext("2d");
 
 
-
+let prevMouseX, prevMouseY, snapshot;
 let isDrawing = false;
+let selectedTool = "brush";
+let burshWidth = 5;
 
 window.addEventListener("load", () => {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
 });
 
-const startDraw = () => {
+const drawRectangle = (e) => {
+    ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+}
+
+const startDraw = (e) => {
     isDrawing = true;
+    prevMouseX = e.offsetX;
+    prevMouseY = e.offsetY;
     ctx.beginPath();
+    ctx.lineWidth = burshWidth;
+    snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    
 }
 
 const stopDraw = () => {
     isDrawing = false;
+    
 }
 
 const drawing = (e) => {
     if(!isDrawing) return;
-    ctx.lineTo(e.offsetX, e.offsetY); // creating line according to the mouse
-    ctx.stroke(); // drawing/filling line with colour
+    ctx.putImageData(snapshot, 0, 0);
+
+
+    if(selectedTool ===  "brush"){
+        ctx.lineTo(e.offsetX, e.offsetY); // creating line according to the mouse
+        ctx.stroke(); // drawing/filling line with colour
+    }else if(selectedTool === "rectangle"){
+        drawRectangle(e);
+    }
 }
+
+toolButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        document.querySelector(".options .active").classList.remove("active");
+        button.classList.add("active");
+        selectedTool = button.id;
+        console.log(button.id);
+    })
+})
 
 //const clearCanvasButton = document.getElementsByClassName("clear-canvas");
 //clearCanvasButton.addEventListener("click", pressClearCanvas);
